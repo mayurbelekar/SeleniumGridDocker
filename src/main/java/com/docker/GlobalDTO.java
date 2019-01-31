30b95d7ad256bf6254ec1bf1eb643ea68a4a5b06
mayurbelekar <mayurbelekar@hotmail.com>
2019-01-31 16:25:20 +0530

    adding global dto

--- src/main/java/com/docker/GlobalDTO.java
+++ src/main/java/com/docker/GlobalDTO.java
@@ -1,14 +1,13 @@
 package com.docker;
 
-import java.util.List;
-import java.util.Map;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class GlobalDTO {
 
 	private String browser = null;
+	private String browserVersion = null;
+	private String platformName = null;
 	private String mode = null;
 	private String hubUrl = null;
 	private int nodeCount = 0;
@@ -19,6 +18,13 @@ public class GlobalDTO {
 	private String nodeName = null;
 	private String execType = null;
 	private String driverpath = null;
+	private String proxyType = null;
+	private String manualProxyIP = null;
+	private String manualProxyPort = null;
+	private String automaticProxyUrl = null;
+	private String sauceLabsUsername = null;
+	private String sauceLabsAccessKey = null;
+	private String desktopApplication = null;
 	
 	public String getBrowser() {
 		return browser;
@@ -26,6 +32,22 @@ public class GlobalDTO {
 	
 	public void setBrowser(String browser) {
 		this.browser = browser;
+	}
+	
+	public void setBrowserVersion(String browserVersion) {
+		this.browserVersion = browserVersion;
+	}
+	
+	public String getBrowserVersion() {
+		return browserVersion;
+	}
+	
+	public void setPlatformName(String platformName) {
+		this.platformName = platformName;
+	}
+	
+	public String getPlatformName() {
+		return platformName;
 	}
 	
 	public void setMode(String mode) {
@@ -107,4 +129,61 @@ public class GlobalDTO {
 	public String getDriverPath() {
 		return driverpath;
 	}
+	
+	public void setProxyType(String proxyType) {
+		this.proxyType = proxyType;
+	}
+	
+	public String getProxyType() {
+		return proxyType;
+	}
+	
+	public void setManualProxyIP(String manualProxyIP) {
+		this.manualProxyIP = manualProxyIP;
+	}
+	
+	public String getManualProxyIP() {
+		return manualProxyIP;
+	}
+	
+	public void setManualProxyPort(String manualProxyPort) {
+		this.manualProxyPort = manualProxyPort;
+	}
+	
+	public String getManualProxyPort() {
+		return manualProxyPort;
+	}
+	
+	public void setAutomaticProxyUrl(String automaticProxyUrl) {
+		this.automaticProxyUrl = automaticProxyUrl;
+	}
+	
+	public String getAutomaticProxyUrl() {
+		return automaticProxyUrl;
+	}
+	
+	public void setSauceLabsUsername(String sauceLabsUsername) {
+		this.sauceLabsUsername = sauceLabsUsername;
+	}
+	
+	public String getSauceLabsUsername() {
+		return sauceLabsUsername;
+	}
+	
+	public void setSauceLabsAccessKey(String sauceLabsAccessKey) {
+		this.sauceLabsAccessKey = sauceLabsAccessKey;
+	}
+	
+	public String getSauceLabsAccessKey() {
+		return sauceLabsAccessKey;
+	}
+	
+	public void setDesktopApplication(String desktopApplication) {
+		this.desktopApplication = desktopApplication;
+	}
+	
+	public String getDesktopApplication() {
+		return desktopApplication;
+	}
+	
 }
